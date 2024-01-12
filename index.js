0c4e336de432615443f6c55e40100b31c484bb91
enochtagoe <enochtagoe@pursuit.org>
2024-01-12 11:51:20 -0500

    personB

---
+++ index.js
@@ -0,0 +1,11 @@
+function createPerson(name, age) {
+  return {
+    name,
+    age,
+    friends: [],
+  };
+}
+function addPersonAsFriend(personA, personB) {
+  personA.friends.push(personB);
+  return personA;
+}
